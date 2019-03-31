97e34486aa73d7c7e4e3b5f683cb15c3222e3d89
the-l <the-l@DESKTOP-PKO003Q>
2019-03-31 14:28:16 -0400

    finish deploy script

--- re-deploy.ps1
+++ re-deploy.ps1
@@ -1,18 +1,35 @@
-﻿cd "C:\temp\Jan-21-2019\orion-web-master"
+﻿#manual start location for testing
+#cd "C:\temp\build-Jan-22-2019"
+
 #stop services
 $services=$(docker ps -q)
 if ($services) { docker stop $(docker ps -q) }
-#set tls level so we can download the zip
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://github.com/Anoobus/orion-web/archive/master.zip" -OutFile "..\..\orion-web-master.zip"
+
 #get date identifier
 $dateId=Get-Date -Format "MMM-dd-yyyy"
+$dateId="build-$dateId"
+
+#back up db folder
+$backupDbPath=Get-Location
+Copy-Item "..\sql-mount" -Destination "$backupDbPath-sql-mount-backup" -recurse
+
+#set tls level so we can download the zip
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://github.com/Anoobus/orion-web/archive/master.zip" -OutFile "..\orion-web-master.zip"
+
 #unzip the file
-Expand-Archive -LiteralPath "..\..\orion-web-master.zip" -DestinationPath "..\..\$dateId"
+Expand-Archive -LiteralPath "..\orion-web-master.zip" -DestinationPath "..\temp"
+Move-Item -Path "..\temp\orion-web-master" -Destination "..\$dateId"
+
 #copy over the .env file
-Copy-Item ".\.env" -Destination "..\..\$dateId\orion-web-master"
+Copy-Item ".\.env" -Destination "..\$dateId"
+
 #delete old zip folder
-Remove-Item "..\..\orion-web-master.zip"
+Remove-Item "..\orion-web-master.zip"
+Remove-Item "..\temp" -Recurse
+
 #set working dir to "new folder"
-cd "..\..\$dateId\orion-web-master"
+cd "..\$dateId"
+
+#start app and sql
 docker-compose up -d
