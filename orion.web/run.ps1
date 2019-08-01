f7cc5692f7216ca70f965545c7c9195058163c31
Timothy Thelen <t.thelen@criteo.com>
2019-07-31 21:41:42 -0400

    more logging on startup

--- orion.web/run.ps1
+++ orion.web/run.ps1
@@ -1,4 +1,4 @@
-﻿
+﻿cd app
 $Env:ASPNETCORE_URLS = "https://+;http://+"
 $Env:ASPNETCORE_Kestrel__Certificates__Default__Path = (Get-ChildItem "self-cert.pfx").FullName
 $Env:ASPNETCORE_ENVIRONMENT = "VM"
