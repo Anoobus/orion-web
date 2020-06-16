6747d0a558bf0c9eaf916dc9b18cdda79a833596
Timothy Thelen <t.thelen@criteo.com>
2020-06-15 20:26:30 -0400

    Remove Client Code From DB

--- orion.web/DataAccess/EF/Migrations/20200616002612_RemoveClientCode.Designer.cs
+++ orion.web/DataAccess/EF/Migrations/20200616002612_RemoveClientCode.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using orion.web.DataAccess.EF;
 
 namespace orion.web.DataAccess.EF.Migrations
 {
     [DbContext(typeof(OrionDbContext))]
-    partial class OrionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200616002612_RemoveClientCode")]
+    partial class RemoveClientCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ orion.web/DataAccess/EF/Migrations/20200616002612_RemoveClientCode.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace orion.web.DataAccess.EF.Migrations
+{
+    public partial class RemoveClientCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientCode",
+                table: "Clients");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientCode",
+                table: "Clients",
+                nullable: true);
+        }
+    }
+}
